741dbc18f1bd87b14e71751411f3782d1be2c618
Eric Williams <eric.williams.neu@gmail.com>
2019-02-18 20:35:34 -0600

    Merge all feature
    Merging all features from separate folders into Beta 1.3
    
    Co-Authored-By: sunnyliu1220 <sunnyliu1220@users.noreply.github.com>
    Co-Authored-By: optimistic-trousers <optimistic-trousers@users.noreply.github.com>
    Co-Authored-By: kevinjwoods <kevinjwoods@users.noreply.github.com>
    Co-Authored-By: Kevin Gray <kevin.gray.developer@gmail.com>
    Co-Authored-By: DavidStim <dscode14@users.noreply.github.com>

--- Beta 1.3/src/main/java/frc/robot/Robot.java
+++ Beta 1.3/src/main/java/frc/robot/Robot.java
@@ -25,17 +25,12 @@ import com.ctre.phoenix.motorcontrol.*;
 
 // ----------------------------------------------------------------------------------------
 
-public class Robot extends IterativeRobot 
+public class Robot extends TimedRobot
 {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
-/* Allignment variables */
-  boolean center;
-  String driveDirection = "Left";
-  double strafeSpeed = 0.6;
 
 /* Drive motor Voltage Variables. */
   double voutFR;
@@ -47,49 +42,78 @@ public class Robot extends IterativeRobot
   double xaxis;
   double yaxis;
   double rotation;
-  double deadzone = 0.20; 
+  double deadzone = 0.20;
   boolean liftup;
   boolean liftdown;
+
+/* Manual drive variables */
+  double driveXAxis;
+  double driveYAxis;
+  double driveRotation;
 
 /* Ultrasonic Straightening Variables */
   double USSLout;
   double USSRout;
   double lineUS;
 
+  /* Alignment Variables */
   double gyroangle;
   boolean lineup;
-  boolean lineupfinish;
   boolean gyromove;
+  boolean center = false;
+  private double centerStrafeSpeed = 0.7;
+  private double centerReverseSpeed = 0.4;
+  private double centerReverseSpeed2 = 0.38;
+  private int centerIterations = 10;//how many iterations to reverse
+  private int centerIterations2 = 10;
+  private int centerIterationsCount = 0;//the count of iterations to reverse
+  //Color sensor error estimate
+  private int alphaFirst = 0;
+
+  /* Automated Drive Variables */
+  private double tempXAxis = 0;
+  private double tempYAxis = 0;
+  private double tempRotation = 0;
+
+  /* Robot States */
+  private String driveState = "normal";
+  private String centerState = "start";
+  private String lineupState = "start";
+  private String centerDirection = "left";
+  private int gyroDirection = 4;//facing front
+
+
 
 /* Joystick 1 Control variables */
   Joystick joy1 = new Joystick(0);
-  boolean slowdown;
-  double slowmodifer;
+  boolean slowdown = false;
+  double slowmodifer = 1.0;
 
   Joystick joy2 = new Joystick(1);
 
-/* Alignment Variables */
-  int CMD = 0x80;
-  int MULTI_BYTE_BIT = 0x20;
-  int ENABLE_REGISTER  = 0x00;
-  int ATIME_REGISTER   = 0x01;
-  int PPULSE_REGISTER  = 0x0E;
+/* Color Sensor API Variables */
+  final int CMD = 0x80;
+  final int MULTI_BYTE_BIT   = 0x20;
+  final int ENABLE_REGISTER  = 0x00;
+  final int ATIME_REGISTER   = 0x01;
+  final int PPULSE_REGISTER  = 0x0E;
 
-  int ID_REGISTER     = 0x12;
-  int CDATA_REGISTER  = 0x14; 
+  final int ID_REGISTER     = 0x12;
+  final int CDATA_REGISTER  = 0x14;
 
-  int PON   = 0b00000001;
-  int AEN   = 0b00000010;
-  int PEN   = 0b00000100;
-  int WEN   = 0b00001000;
-  int AIEN  = 0b00010000;
-  int PIEN  = 0b00100000;
+  final int PON   = 0b00000001;
+  final int AEN   = 0b00000010;
+  final int PEN   = 0b00000100;
+  final int WEN   = 0b00001000;
+  final int AIEN  = 0b00010000;
+  final int PIEN  = 0b00100000;
 
   private final double integrationTime = 10;
+  private final short threshold = 25;
   private ByteBuffer buffer = ByteBuffer.allocate(10);
-  private short alpha = 0, threshold = 25;
-  
-  I2C sensor = new I2C(I2C.Port.kOnboard, 0x39);
+  private short alpha = 0; 
+
+  I2C colorSensor = new I2C(I2C.Port.kOnboard, 0x39);
 
 // Everything Else
   AHRS ahrs = new AHRS(SerialPort.Port.kUSB1);
@@ -97,7 +121,7 @@ public class Robot extends IterativeRobot
 
   AnalogInput sensorL = new AnalogInput(0);
   AnalogInput sensorR = new AnalogInput(1);
- 
+
   WPI_TalonSRX talonFR = new WPI_TalonSRX(3);
   WPI_TalonSRX talonBR = new WPI_TalonSRX(4);
 
@@ -106,18 +130,19 @@ public class Robot extends IterativeRobot
 
   WPI_TalonSRX liftmotor = new WPI_TalonSRX(5);
 
- MecanumDrive mecdrive = new MecanumDrive(talonFL, talonBL, talonFR, talonBR);
-  Compressor c = new Compressor(0);
-  DoubleSolenoid Solenoid1 = new DoubleSolenoid(1, 3);
+  MecanumDrive mecdrive = new MecanumDrive(talonFL, talonBL, talonFR, talonBR);
+  
+  Compressor compressor = new Compressor(0);
+  DoubleSolenoid ejectorSolenoid = new DoubleSolenoid(1, 3);
   DoubleSolenoid Solenoid2 = new DoubleSolenoid(0, 2);
 // ----------------------------------------------------------------------------------------
   @Override
-  public void robotInit() 
+  public void robotInit()
   {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    
+
     /* Initialize and Configure Cameras */
     UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture(0);
     camera1.setResolution(640, 400);
@@ -126,28 +151,48 @@ public class Robot extends IterativeRobot
     camera1.setFPS(24);
     camera2.setFPS(24);
 
-    // Allignment Preliminary Code
+    // Color Sensor Startup Code
     buffer.order(ByteOrder.LITTLE_ENDIAN);
-    sensor.write(CMD | 0x00, PON | AEN | PEN);
-    sensor.write(CMD | 0x01, (int) (256-integrationTime/2.38)); 
-    sensor.write(CMD | 0x0E, 0b1111);
+    colorSensor.write(CMD | 0x00, PON | AEN | PEN);
+    colorSensor.write(CMD | 0x01, (int) (256-integrationTime/2.38));
+    colorSensor.write(CMD | 0x0E, 0b1111);
+
+ /* 
+    motor.setSensorPhase(true);
+    motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    motor.config_kP(0, 0.01, 30);
+    motor.config_kI(0, 0.25, 30);
+    motor.config_kD(0, 0.015, 30);
+    motor.config_kF(0, 0.1, 30);
+    motor.configClosedLoopPeriod(0, 1, 30);
+    motor.configClearPositionOnQuadIdx(clear, 20);
+  */
+
+   //NavX
+   try
+   {
+     ahrs = new AHRS(SerialPort.Port.kUSB1);
+   }
+   catch(RuntimeException ex)
+   {
+     DriverStation.reportError("NavX Setup Error" + ex.getMessage(),true);
+   }
   }
 // ----------------------------------------------------------------------------------------
   @Override
-  public void robotPeriodic() 
+  public void robotPeriodic()
   {
-    
+
     SmartDashboard.putNumber("alphavalue", alpha);
     center = joy2.getRawButton(5);
     slowdown = joy1.getRawButton(1);
     lineup = joy1.getRawButton(2);
     gyromove = joy1.getRawButton(3);
-    
+
     ZRotation = ahrs.getAngle();
-    
+
     SmartDashboard.putBoolean("slowmode", slowdown);
     SmartDashboard.putBoolean("lineup", lineup);
-    SmartDashboard.putBoolean("lineupfinish", lineupfinish);
 
     SmartDashboard.putNumber("Yaw", ZRotation);
     SmartDashboard.putBoolean("is connected", ahrs.isConnected());
@@ -166,6 +211,8 @@ public class Robot extends IterativeRobot
     talonBR.configOpenloopRamp(0.20, 20);
 
     /* Deadzone Logic */
+    //Potentially replace with setDeadBand()
+    //mecdrive.setDeadband(0.2);
     if (joy1.getRawAxis(0) > deadzone || joy1.getRawAxis(0) < -deadzone)
     {
       xaxis = joy1.getRawAxis(0);
@@ -190,19 +237,26 @@ public class Robot extends IterativeRobot
     {
       rotation = 0;
     }
+
+    /* 
+    position = motor.getSelectedSensorPosition();
+    velocity = motor.getSelectedSensorVelocity();
+    */
+
+    alpha = readAlphaIntensity();
   }
   // ----------------------------------------------------------------------------------------
   @Override
-  public void autonomousInit() 
+  public void autonomousInit()
   {
     m_autoSelected = m_chooser.getSelected();
     System.out.println("Auto selected: " + m_autoSelected);
   }
   // ----------------------------------------------------------------------------------------
   @Override
-  public void autonomousPeriodic() 
+  public void autonomousPeriodic()
   {
-    switch (m_autoSelected) 
+    switch (m_autoSelected)
     {
       case kCustomAuto:
 
@@ -215,30 +269,16 @@ public class Robot extends IterativeRobot
   }
    // ----------------------------------------------------------------------------------------
   @Override
-  public void teleopPeriodic() 
+  public void teleopPeriodic()
   {
-    /* Lift Logic */
-    if (liftup == true)
-    {
-      liftmotor.set(0.7);
-    }
-    else if (liftdown == true)
-    {
-      liftmotor.set(-0.7);
-    }
-    else
-    {
-      liftmotor.set(0.0);
-    }
-
     /* Pnumatics Logic */
     if(joy2.getRawButton(1))
     {
-    Solenoid1.set(DoubleSolenoid.Value.kForward);
+    ejectorSolenoid.set(DoubleSolenoid.Value.kForward);
     }
     else if(joy2.getRawButton(2))
     {
-    Solenoid1.set(DoubleSolenoid.Value.kReverse);
+    ejectorSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
     if(joy2.getRawButton(3))
     {
@@ -248,31 +288,126 @@ public class Robot extends IterativeRobot
     {
     Solenoid2.set(DoubleSolenoid.Value.kReverse);
     }
-    
 
-    if (slowdown == true)
+     //drive state switcher
+     if (driveState.equals("center"))
+     {
+       if (!center || centerState.equals("stop"))
+       {
+         tempXAxis = 0;
+         mecdrive.driveCartesian(0,0,0);
+         driveState = "normal";
+         centerState = "start";
+       }
+     }
+     else if (driveState.equals("lineup"))
+     {
+       if (!lineup || lineupState.equals("stop"))
+       {
+         tempXAxis = 0;
+         tempYAxis = 0;
+         tempRotation = 0;
+         mecdrive.driveCartesian(0,0,0);
+         driveState = "normal";
+         lineupState = "start";
+       }
+     }
+     //no exit conditions and operations
+    else
     {
-      slowmodifer = 0.5;
+      if (center && joy1.getRawButtonPressed(3))//X
+      {
+        driveState = "center";
+        centerDirection = "left";
+      }
+      else if (center && joy1.getRawButtonPressed(2))//B
+      {
+        driveState = "center";
+        centerDirection = "right";
+      }
+      else if (lineup && joy1.getRawButtonPressed(4))//Y
+      {
+        driveState = "lineup";
+      }
+      /*
+      placeholders
+      */
+      else if (joy1.getRawButtonPressed(1))//A, placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 1;//back right rocket
+      }
+      else if (false)//placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 2;//right rocket
+      }
+      else if (false)//placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 3;//front right rocket
+      }
+      else if (false)//placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 4;//front
+      }
+      else if (false)//placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 5;//front left rocket
+      }
+      else if (false)//placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 6;//left rocket
+      }
+      else if (false)//placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 7;//back left rocket
+      }
+      else if (false)//placeholder for potential touch screen control
+      {
+        driveState = "fixedOrientation";
+        gyroDirection = 8;//loading station
+      }
     }
-    else if (slowdown == false)
+
+    switch (driveState)
     {
-      slowmodifer = 1.0;
-    }
-    
-    if (lineup == true)
-    {
-      USCorrection();
-    }
-    if (center == true)
-    {
-      CenterRobot();
-    }
-      mecdrive.driveCartesian(xaxis * slowmodifer, -yaxis * slowmodifer, rotation * slowmodifer);
-      lineupfinish = false;
+      //normal drive state
+      case "normal":
+      {
+        driveNormal();
+        break;
+      }
+
+      //center state
+      case "center":
+      {
+        driveCenter();
+        break;
+      }
+
+      //lineup state
+      case "lineup":
+      {
+        driveLineup();
+        break;
+      }//end of lineup
+
+      //gyro fixed orientation state
+      case "fixedOrientation":
+      {
+        driveFixedOrientation();
+        break;
+      }
+    }//end of switch
   }
     // ----------------------------------------------------------------------------------------
   @Override
-  public void testPeriodic() 
+  public void testPeriodic()
   {
 
   }
@@ -295,41 +430,248 @@ public class Robot extends IterativeRobot
     {
       rotation = -0.175 + lineUS;
     }
-  } 
- /* Lightsensor Lineup Function */
-  public void CenterRobot()
-  {
-    if (driveDirection == "Left" && alpha < threshold)
-    {
-      yaxis = -strafeSpeed;
-      if (alpha > threshold)
-      {
-        driveDirection = "Right";
-        yaxis = 0;
-        if (strafeSpeed/2 <= 0.2) {strafeSpeed = strafeSpeed/2 ;};
-      }
-    }
-    else if (driveDirection == "Right" && alpha < threshold)
-    {
-      yaxis = strafeSpeed;
-      if (alpha > threshold)
-      {
-        driveDirection = "Left";
-        yaxis = 0;
-        if (strafeSpeed/2 >= 0.2) {strafeSpeed = strafeSpeed/2; }
-      }
-    }
-    else
-    {
-      yaxis = 0;
-    }
   }
-  public short readAlphaIntensity() 
+ 
+  public short readAlphaIntensity()
   {
     buffer.clear();
-    sensor.read(CMD | MULTI_BYTE_BIT | CDATA_REGISTER, 10, buffer);
+    colorSensor.read(CMD | MULTI_BYTE_BIT | CDATA_REGISTER, 10, buffer);
     short alpha_value = buffer.getShort(0);
     if(alpha_value < 0) { alpha_value += 0b10000000000000000; }
     return alpha_value;
   }
+
+  public void driveNormal()
+  {
+    mecdrive.setDeadband(0.2);
+    if (slowdown)
+    {
+      slowmodifer = 0.5;
+    }
+    else
+    {
+      slowmodifer = 1.0;
+    }
+
+    driveXAxis = joy1.getRawAxis(0);
+    driveYAxis = joy1.getRawAxis(1);
+    driveRotation = joy1.getRawAxis(4);
+    mecdrive.driveCartesian(driveXAxis * slowmodifer, -driveYAxis * slowmodifer, driveRotation * slowmodifer);
+  }
+
+  public void driveFixedOrientation()
+  {
+    mecdrive.setDeadband(0.2);
+    if (slowdown)
+    {
+      slowmodifer = 0.5;
+    }
+    else
+    {
+      slowmodifer = 1.0;
+    }
+
+    driveXAxis = joy1.getRawAxis(0);
+    driveYAxis = joy1.getRawAxis(1);
+    double error = 0;
+    double angle = 0;
+    double heading = ahrs.getAngle();
+    double kp = 0.03;
+    if (gyroDirection == 1)
+    {
+      angle = 61.25;
+    }
+    else if (gyroDirection == 2)
+    {
+      angle = 0;
+    }
+    else if (gyroDirection == 3)
+    {
+      angle = 298.75;
+    }
+    else if (gyroDirection == 4)
+    {
+      angle = 90;
+    }
+    else if (gyroDirection == 5)
+    {
+      angle = 241.25;
+    }
+    else if (gyroDirection == 6)
+    {
+      angle = 180;
+    }
+    else if (gyroDirection == 7)
+    {
+      angle = 118.75;
+    }
+    else if (gyroDirection == 8)
+    {
+      angle = 270;
+    }
+
+    error = heading - angle;
+    while (error >= 180)
+    {
+      error -= 360;
+    }
+    while (error < -180)//Must be < not <=
+    {
+      error += 360;
+    }
+    mecdrive.driveCartesian(driveXAxis * slowmodifer, -driveYAxis * slowmodifer, (0.2 + error*kp) * slowmodifer, error);
+  }
+
+  public void driveCenter()
+  {
+    /*
+     * The robot will only center if it was not on the line previously.
+     * This (neverOnLine) is reset to true if the button is released.
+     * Strafe speed works at -0.5, reverse speed at 0.5, and iterations at 14 for perfect accuracy.
+     * There is room for improvement for speed.
+     * 
+     * For strafe speed at -0.6, reverse speed at 0.38, and iterations at 25.
+     * Not as accurate as the previous but could potentially be improved to the same accuracy.
+     */
+
+    switch (centerState)
+    {
+      //start centering
+      case "start":
+      {
+        if (alpha >= threshold)
+        {
+          alphaFirst = alpha;
+          centerState = "reverse";
+        }
+  
+        else
+        {
+          mecdrive.setDeadband(0);
+          if (centerDirection.equals("left"))
+          {
+            tempXAxis = -centerStrafeSpeed;
+          }
+          else if (centerDirection.equals("right"))
+          {
+            tempXAxis = centerStrafeSpeed;
+          }
+        }
+
+        break;
+      }
+
+      //reverse state
+      case "reverse":
+      {
+        if (centerIterationsCount >= centerIterations)
+        {
+          centerIterationsCount = 0;
+          mecdrive.driveCartesian(0,0,0);
+          centerState = "come_back";
+        }
+  
+        else
+        {
+          if (centerDirection.equals("left"))
+          {
+            tempXAxis = centerReverseSpeed;
+          }
+          else if (centerDirection.equals("right"))
+          {
+            tempXAxis = -centerReverseSpeed;
+          }
+          centerIterationsCount += 1;
+        }
+
+        break;
+      }//end of reverse
+
+      //come back until line again
+      case "come_back":
+      {
+        if (alpha >= threshold)
+        {
+          centerState = "reverse2";
+        }
+  
+        else
+        {
+          if (centerDirection.equals("left"))
+          {
+            tempXAxis = centerReverseSpeed;
+          }
+          else if (centerDirection.equals("right"))
+          {
+            tempXAxis = -centerReverseSpeed;
+          }
+        }
+
+        break;
+      }
+
+      //second reverse
+      case "reverse2":
+      {
+        if (centerIterationsCount >= centerIterations2)
+        {
+          centerIterationsCount = 0;
+          mecdrive.driveCartesian(0,0,0);
+          centerState = "stop";
+        }
+  
+        else
+        {
+          if (centerDirection.equals("left"))
+          {
+            tempXAxis = -centerReverseSpeed2;
+          }
+          else if (centerDirection.equals("right"))
+          {
+            tempXAxis = centerReverseSpeed2;
+          }
+          centerIterationsCount += 1;
+        }
+
+        break;
+      }
+    }//end of switch
+
+    mecdrive.driveCartesian(tempXAxis,tempYAxis,tempRotation);
+  }//end of driveCenter()
+
+  public void driveLineup()
+  {
+    switch (lineupState)
+    {
+      //start lineup
+      case "start":
+      {
+        if (Math.abs(USSLout - USSRout) <= 2)
+        {
+          mecdrive.driveCartesian(0,0,0);
+          lineupState = "stop";
+        }
+
+        else
+        {
+          mecdrive.driveCartesian(0,0,0);
+          lineUS = ((USSLout - USSRout) / 500);
+
+          if (USSLout > USSRout)
+          {
+            tempRotation = 0.175 + lineUS;
+          }
+          else
+          {
+            tempRotation = -0.175 + lineUS;
+          }
+        }
+
+        break;
+      }
+    }//end of switch
+
+    mecdrive.driveCartesian(tempXAxis,tempYAxis,tempRotation);
+  }//end of driveLineup()
 }
