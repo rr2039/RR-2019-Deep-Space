a65d4aa4a391bc6f43f28bc6805cba4f37f69756
Kevin Gray <graykevinb@gmail.com>
2019-06-18 20:19:03 -0500

    Switched to entirely new code

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class RobotMap {
+
+  public static WPI_TalonSRX talonFL = new WPI_TalonSRX(0);
+  public static WPI_TalonSRX talonBL = new WPI_TalonSRX(3);
+  public static WPI_TalonSRX talonFR = new WPI_TalonSRX(15);
+  public static WPI_TalonSRX talonBR = new WPI_TalonSRX(13);
+
+
+  public static WPI_TalonSRX liftMotor = new WPI_TalonSRX(14);
+  public static  WPI_TalonSRX wristMotor = new WPI_TalonSRX(9);
+}
